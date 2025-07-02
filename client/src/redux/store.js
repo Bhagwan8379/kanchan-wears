32df269d728d2f53ffcdc22b00568ea3562905ec
Bhagwan <bhagwangire05@gmail.com>
2025-07-02 16:12:59 +0530

    store updated

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,14 +1,30 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { authApi } from "./api/authApi";
-import authSlice from "./slice/authSlice";
+// import { configureStore } from "@reduxjs/toolkit";
+// import { authApi } from "./api/authApi";
+// import authSlice from "./slice/authSlice";
 
+
+// const reduxStore = configureStore({
+//     reducer: {
+//         [authApi.reducerPath]: authApi.reducer,
+//         auth: authSlice
+//     },
+//     middleware: def => [def(), authApi.middleware]
+// })
+
+// export default reduxStore
+
+
+
+
+import { configureStore } from "@reduxjs/toolkit";
+import { authApi } from './api/authApi'
+import authSlice from './slice/authSlice'
 
 const reduxStore = configureStore({
     reducer: {
         [authApi.reducerPath]: authApi.reducer,
         auth: authSlice
     },
-    middleware: def => [def(), authApi.middleware]
 })
 
 export default reduxStore
